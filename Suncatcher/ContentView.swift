f2102f032e4a4e24820cf82a14c4176a979d14c4
jack <52892975+jackkroll@users.noreply.github.com>
2026-04-28 14:02:51 -0400

    Initial Commit

---
+++ Suncatcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Suncatcher
+//
+//  Created by Jack Kroll on 4/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Suncatcher/SuncatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuncatcherApp.swift
+//  Suncatcher
+//
+//  Created by Jack Kroll on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct SuncatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
